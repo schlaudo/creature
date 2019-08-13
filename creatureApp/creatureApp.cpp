0e074edafded042ec592a2f82d11aeed365f4ac9
schlaudo <54037920+schlaudo@users.noreply.github.com>
2019-08-13 11:33:21 +0200

    update main program

--- creatureApp/creatureApp.cpp
+++ creatureApp/creatureApp.cpp
@@ -8,42 +8,76 @@
 #include <iostream>
 using namespace std;
 
+
+void showMenu()
+{
+	cout << endl;
+	cout << "Mann ............. 1" << endl;
+	cout << "Frau ............. 2" << endl;
+	cout << "Bartagame ........ 3" << endl;
+	cout << "Programmende ..... 9" << endl << endl;
+	cout << "Ihre Wahl: ";
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	HumanBeing*	pHuman = new HumanBeing(71, 175, "Mann", 2, 2);
-	//cout << "Gewicht: " << pHuman->getWeight() << endl;
-	cout << endl;
-	pHuman->showAttributes();
+	HumanBeing*	pHumanBeing;
+	Reptile*	pReptile;
+	int			selection;
 
-	cout << endl;
-	pHuman->moveOn();
+	do
+	{
+		showMenu();
+		cin >> selection;
+		cout << endl;
 
-	cout << endl;
-	delete pHuman;
+		pHumanBeing = NULL;
+		pReptile = NULL;
 
-	cout << endl;
+		switch (selection)
+		{
+		case 1:
+			pHumanBeing = new HumanBeing(75, 182, "Mann", 2, 2);
+			break;
+		case 2:
+			pHumanBeing = new HumanBeing(53, 167, "Frau", 2, 2);
+			break;
+		case 3:
+			pReptile = new Reptile(1, 30, "Bartagame", 4);
+			break;
+		case 9:
+			cout << endl << "Programmende!" << endl;
+			break;
+		default:
+			cout << endl << "Falsche Eingabe!" << endl << endl;
+			break;
+		}
 
-	Animal* pAnimal = new Animal(21, 45, "Hund", 4);
-	//cout << "Gewicht: " << pAnimal->getWeight() << endl;
-	cout << endl;
-	pAnimal->showAttributes();
+		cout << endl;
+		//now we have a lot of code,
+		//100 pages later on ...
+		if (pHumanBeing != NULL)
+			pHumanBeing->showAttributes();
+		else if (pReptile != NULL)
+			pReptile->showAttributes();
 
-	cout << endl;
-	pAnimal->moveOn();
+		cout << endl;
+		//... again there is a lot of code,
+		//150 pages later ...
+		if (pHumanBeing != NULL)
+			pHumanBeing->moveOn();
+		else if (pReptile != NULL)
+			pReptile->moveOn();
 
-	cout << endl;
-	delete pAnimal;
+		cout << endl;
+		//... 50 pages later ...
+		//don't forget to delete the objects
+		if (pHumanBeing != NULL)
+			delete pHumanBeing;
+		else if (pReptile != NULL)
+			delete pReptile;
 
-	Reptile* pReptile = new Reptile(1, 30, "Bartagame", 4);
-	//cout << "Gewicht: " << pReptile->getWeight() << endl;
-	cout << endl;
-	pReptile->showAttributes();
-
-	cout << endl;
-	pReptile->moveOn();
-
-	cout << endl;
-	delete pReptile;
+	} while (selection != 9);
 
 	return 0;
 }
